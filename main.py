e20902535c9bab18c070b28e5976860cd08e1d76
guisena08 <guisena@ufc.alu.br>
2023-04-06 20:23:55 -0300

    Calculadora de idade no futuro completa.

---
+++ main.py
@@ -0,0 +1,6 @@
+ano_atual = int(input("digite o ano atual:"))
+ano_desejado = int(input("digite o ano desejado:"))
+idade = int(input("digite sua idade:"))
+nome = input("digite seu nome:")
+futuro = ano_desejado - ano_atual
+print(nome, "no ano de",ano_desejado, "você terá",futuro + idade, "anos")
