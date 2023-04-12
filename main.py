e3336c87c3258ce12680c8b583518be4da2cc4cc
guisena08 <guisena@ufc.alu.br>
2023-04-11 22:11:55 -0300

    problema da virgula solucionado

--- main.py
+++ main.py
@@ -2,5 +2,6 @@ ano_atual = int(input("digite o ano atual:"))
 ano_desejado = int(input("digite o ano desejado:"))
 idade = int(input("digite sua idade:"))
 nome = input("digite seu nome:")
-print(nome,",", "no ano de",ano_desejado, "você terá", (ano_desejado - ano_atual) + idade , "anos")
+virgula = "," 
+print(nome + virgula, "no ano de",ano_desejado, "você terá", (ano_desejado - ano_atual) + idade , "anos")
 
