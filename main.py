bec6d742feb723c93489fd95ccf67bc43707dd4e
guisena08 <guisena@ufc.alu.br>
2023-04-11 22:04:12 -0300

    resolvendo erro

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ ano_atual = int(input("digite o ano atual:"))
 ano_desejado = int(input("digite o ano desejado:"))
 idade = int(input("digite sua idade:"))
 nome = input("digite seu nome:")
-print(nome, "no ano de",ano_desejado, "você terá", (ano_desejado - ano_atual) + idade , "anos")
+print(nome,",", "no ano de",ano_desejado, "você terá", (ano_desejado - ano_atual) + idade , "anos")
 
