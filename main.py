bd75f83cdfa290af62b8f6495b8356f7f96d996d
guisena08 <guisena@alu.ufc.br>
2023-04-12 18:53:56 -0300

    concertando erro de ordem

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 ano_atual = int(input("digite o ano atual:"))
-ano_desejado = int(input("digite o ano desejado:"))
 idade = int(input("digite sua idade:"))
+ano_desejado = int(input("digite o ano desejado:"))
 nome = input("digite seu nome:")
 virgula = "," 
 print(nome + virgula, "no ano de",ano_desejado, "você terá", (ano_desejado - ano_atual) + idade , "anos")
+
 
